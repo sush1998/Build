83cc5329249e21ab17baffaea7f76e721bc02373
SUSHANT CHANDRASHEKHAR MAHALLE <mahallesushant1998@gmail.com>
2021-10-01 23:42:59 +0530

    feat: change size on click

---
+++ problems/buildTwo/script2.js
@@ -0,0 +1,25 @@
+const input=document.querySelector("#input")
+const btnClick=document.querySelector("#btn-text")
+const btnPlus=document.querySelector("#btn-plus")
+const btnMinus=document.querySelector("#btn-minus")
+const output=document.querySelector("#outputText")
+var number=16
+
+btnClick.addEventListener("click",()=>
+{
+   console.log("clicked")
+   output.innerHTML=input.value;
+})
+btnPlus.addEventListener("click",()=>
+{
+    number+=2
+    output.style.fontSize=number+'px'
+    console.log(number)
+})
+
+btnMinus.addEventListener("click",()=>
+{
+    number-=2
+    output.style.fontSize=number+'px'
+    console.log(number)
+})
